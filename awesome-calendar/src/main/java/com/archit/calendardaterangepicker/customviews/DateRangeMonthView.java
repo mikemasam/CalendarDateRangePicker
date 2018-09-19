9a494bc5e61a792d77b2d14c9cf9da4d8c6e9e5d
Meshack Masam <meshackmasam@gmail.com>
2018-09-20 01:26:00 +0300

    past months issue fixed

--- awesome-calendar/src/main/java/com/archit/calendardaterangepicker/customviews/DateRangeMonthView.java
+++ awesome-calendar/src/main/java/com/archit/calendardaterangepicker/customviews/DateRangeMonthView.java
@@ -264,7 +264,8 @@ public class DateRangeMonthView extends LinearLayout {
 
         if (currentCalendarMonth.get(Calendar.MONTH) != calendar.get(Calendar.MONTH)) {
             hideDayContainer(container);
-        } else if (today.after(calendar) && (today.get(Calendar.DAY_OF_YEAR) != calendar.get(Calendar.DAY_OF_YEAR))) {
+        } else if (today.after(calendar) && (today.get(Calendar.DAY_OF_YEAR)
+                != calendar.get(Calendar.DAY_OF_YEAR)) && !calendarStyleAttr.isEnabledPastDates()) {
             disableDayContainer(container);
             container.tvDate.setText(String.valueOf(date));
         } else {
